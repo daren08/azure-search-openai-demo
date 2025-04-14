0440c70b7ea79290335376dcd892aa66e6deacff
dasteadfast08 <139190827+dasteadfast08@users.noreply.github.com>
2025-04-14 09:21:44 +0800

    .

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -11,7 +11,7 @@ const DEFAULT_EXAMPLES: string[] = [
 const GPT4V_EXAMPLES: string[] = [
     "Compare the impact of interest rates and GDP in financial markets.",
     "What is the expected trend for the S&P 500 index over the next five years? Compare it to the past S&P 500 performance",
-    "Can you identify any correlation between oil prices and stock market trends?"
+    "Can you identify any correlation between oil prices and stock market trends? "
 ];
 
 interface Props {
